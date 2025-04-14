66614c2fdc75fa70963284a7936552380eb325cd
Sarah Tiffany <362575441@GoTVDSB.ca>
2025-04-14 14:45:00 -0400

    PUSHG!

--- Game1.cs
+++ Game1.cs
@@ -56,6 +56,7 @@ namespace Topic_1__Adding_content
             base.Update(gameTime);
         }
 
+
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.DodgerBlue);
